3e0560b031739ab49bf4e2947913e4da9720e7f4
alux35 <alux35@gmail.com>
2015-05-02 12:45:45 -0400

    Fixed errors showing on first home page load
    Error showed get variable page did not exist. Adding code to check if it
    exists first before trying to get its value

--- index.php
+++ index.php
@@ -4,19 +4,23 @@
 	include 'includes/header.html';
 	include 'includes/sidebar.html';
 	include 'dbconn.php';
-	//When adding a new page, include it here
-	if($_GET['page'] == "rentalform"){
-		include 'rentalform.php';
-	}
-	else if($_GET['page'] == "home"){
-		include 'home.html';
-	}
-	else if($_GET['page'] == "contact"){
-		include 'contact.php';
+	//When adding a new page, include it here if the page parameter is set
+	if(isset($_GET['page']))
+	{
+		if($_GET['page'] == "rentalform"){
+			include 'rentalform.php';
+		}
+		else if($_GET['page'] == "home"){
+			include 'home.html';
+		}
+		else if($_GET['page'] == "contact"){
+			include 'contact.php';
+		}
 	}
 	else{
 		include 'home.html';
 	}
+	
 	
 	include 'includes/footer.html';
 ?>
