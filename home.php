45b488e421b934c2a596ea072f61e2ff0f2142c5
emexem <mxmalik@gmail.com>
2015-05-03 13:37:35 -0400

    home.php bug fixes

--- home.php
+++ home.php
@@ -1,25 +1,25 @@
     <div class="content">
    		<div><h1>Welcome to Eclipse Rentals</h1></div>
-       <form>
+       <form action="index.php?page=home" method="post">
 		<div class="form_settings">
 			<p><span>Search Customers</span><input type="text" name="cSearch" /></p>
 			<input type="submit" name="Search"/>
 			<p>
 			<?php
-			if(isset($_Post['cSearch']))
+			if(isset($_POST['cSearch']))
 			{
-				$searchTXT = $_REQUEST['cSearch'];
-				$searchQuery = 'SELECT * FROM rentals WHERE CustomerName like %$searchTXT%';
+				$searchTXT = $_POST['cSearch'];
+				$searchQuery = "SELECT * FROM rentals WHERE CustomerName like '%" . $searchTXT . "%'";
 				$result = mysql_query($searchQuery, $conn);
 				
 				if (!$result)
 						die("Could not get results: " . mysql_error());
 					while($records = mysql_fetch_array($result, MYSQL_ASSOC))
 					{
-					echo "Rental ID: {$records[0]} &nbsp;"
-						."Rental Type: {$records[1]} &nbsp;"
-						."Customer Name: {$records[2]} &nbsp;"
-						."Number of Days: {$records[3]} <br />";
+					echo "Rental ID: {$records['rentalID']} &nbsp;"
+						."Rental Type: {$records['rentalType']} &nbsp;"
+						."Customer Name: {$records['CustomerName']} &nbsp;"
+						."Number of Days: {$records['numOfDays']} <br />";
 					}
 			}
 			?>
