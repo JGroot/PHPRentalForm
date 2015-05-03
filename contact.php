e8c7b49f6fbf00d342140ad991884360febd7dab
JGroot <jg105@zips.uakron.edu>
2015-05-03 13:56:23 -0400

    mailer again

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
     <div class="content">
    		<div><h1>Contact Us</h1></div>
    		<div class='form_settings'>
-	       <form class="email" action="mailer.php" method="post">
+	       <form class="email" action="index.php?page=contact" method="post">
 				<p>Name:</p>
 				<input type="text" name="name" />
 				<p>E-mail:</p>
@@ -10,7 +10,8 @@
 				<input type="text" name="subject" />
 				<p>Message:</p>
 				<textarea name="message"></textarea></p>
-				<p style="padding-top: 15px"><input class="send" type="submit" value="Send"></p>
+				<p style="padding-top: 15px"><input class="submit" type="submit" value="submit"></p>
 			</form>
 		</div>
+		<?php include ('mailer.php'); ?>
      </div>
