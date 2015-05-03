120c8444170d9752360842a887a0bc84f831e0de
JGroot <jg105@zips.uakron.edu>
2015-05-03 13:21:21 -0400

    Started contact page

--- contact.php
+++ contact.php
@@ -1,5 +1,16 @@
     <div class="content">
    		<div><h1>Contact Us</h1></div>
-       
-        
+   		<div class='form_settings'>
+	       <form class="email" action="mailer.php" method="post">
+				<p>Name:</p>
+				<input type="text" name="name" />
+				<p>E-mail:</p>
+				<input type="text" name="email" />
+				<p>Subject:</p>
+				<input type="text" name="subject" />
+				<p>Message:</p>
+				<textarea name="message"></textarea></p>
+				<input class="send" type="submit" value="Send">
+			</form>
+		</div>
      </div>
